2d8ca7fb13983020a5e02f6236dcd27ea99e9e44
Hom Dahal <homdahal997@gmail.com>
2024-06-15 11:35:53 -0400

    Some bootstrap mark added for app component and all index css removed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,23 @@ import './App.css'
 function App() {
   return (
     <>
-    
+      <section className="vh-100 gradient-custom">
+        <div className="container py-5">
+          <div className="row d-flex justify-content-center align-items-center text-center">
+          <div className="col col-xl-10">
+          <div className="card">
+          <div className="card-body p-5">
+          <h1>ToDo Lists</h1>
+          </div>
+          </div>
+          </div>
+
+          </div>
+
+        </div>
+
+      </section>
+
     </>
   )
 }
