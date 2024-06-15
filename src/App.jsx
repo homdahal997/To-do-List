4b95e84203cbea8506f1fc95a5a252b8aa02932c
Hom Dahal <homdahal997@gmail.com>
2024-06-15 14:02:09 -0400

    Fetch initial todos from API and set them in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import React, {useReducer, useEffect} from 'react';
+import { initialState, reducer } from './reducers/TodoReducer';
 import './App.css'
 import './assets/css/bootstrap-to-do-list.min.css'
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    // Fetch initial todos from the API
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then((response) => response.json())
+      .then((data) => {
+        const formattedData = data.map((todo) => ({
+          id: todo.id,
+          text: todo.title,
+          complete: todo.completed,
+        }));
+        dispatch({ type: 'SET_TODOS', payload: formattedData });
+      });
+  }, []);
 
   return (
     <>
