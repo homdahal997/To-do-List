86e9a867207b4d05b35a20e1ff03ab0135e91fea
Hom Dahal <homdahal997@gmail.com>
2024-06-15 13:47:38 -0400

    implement useReducer in App Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import './App.css'
 import './assets/css/bootstrap-to-do-list.min.css'
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
       <section className="vh-100 gradient-custom">
         <div className="container py-5">
           <div className="row d-flex justify-content-center align-items-center text-center">
-          <div className="col col-xl-10">
-          <div className="card">
-          <div className="card-body p-5">
-          <h1>ToDo Lists</h1>
-          <h3> This is the place for our form </h3>
-          </div>
-          </div>
-          </div>
+            <div className="col col-xl-10">
+              <div className="card">
+                <div className="card-body p-5">
+                  <h1>ToDo Lists</h1>
+                  <h3> This is the place for our form </h3>
+                </div>
+              </div>
+            </div>
 
           </div>
 
