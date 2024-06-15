83022295ca8bf7d960188b216f91f199d1f5e2e9
Hom Dahal <homdahal997@gmail.com>
2024-06-15 12:38:53 -0400

    Update page title and add form placeholder in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './assets/css/bootstrap-to-do-list.min.css'
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
           <div className="col col-xl-10">
           <div className="card">
           <div className="card-body p-5">
-          <h1>ToDo Lists</h1>
+          <h1>ToDo List</h1>
+          <h3> This is the place for our form </h3>
           </div>
           </div>
           </div>
